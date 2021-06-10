ab2c5162c07d59e451a6ddf24b173f56cc738f1e
scbonner <45452011+scbonner@users.noreply.github.com>
2021-06-10 17:29:38 -0400

    initial save of website reworking first page

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+        </p>
+        <a
+          className="App-link"
+          href="https://www.pexels.com/photo/dad-and-daughter-having-fun-and-laughing-while-looking-at-the-smartphone-6140198/"
+          target="_blank"
+          rel="noopener noreferrer"
+          
+        >
+          CLICK TO ENTER
+        </a>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
