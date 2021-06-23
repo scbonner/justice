00bc62797f610128c84ee5cac88a1bc7f9256a1b
scbonner <45452011+scbonner@users.noreply.github.com>
2021-06-23 05:25:50 -0400

    satisfied with logo position and navbar look

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.png';
-import Staff from './Staff'
+//import Staff from './Staff'
 import Jeannette from './Jeannette/jeannette.js'
-import Partnerships from './Partnerships'
-import Policy from './Policy'
-import Institute from './Institute'
-import Trainings from './Trainings'
-import Home from './Home'
-//import NavBar from './NavBar'
+// import Partnerships from './Partnerships'
+// import Policy from './Policy'
+// import Institute from './Institute'
+// import Trainings from './Trainings'
+// import Home from './Home'
+import NavBar from './NavBar'
 import WhatWeDo from './WhoWeAre'
 import WhoWeAre from './WhatWeDo'
 //import Values from './Values';
@@ -48,54 +48,12 @@ function App() {
   return (
     
     <div className="App">
-        {/* <NavBar /> */}
-        <div>
-            <nav className="navbar navbar sticky-top navbar-expand-md bg=light variant=light mb-5">
-                {/* <nav className="navbar sticky-top navbar-light bg-light"> */}
-                <div className="container-fluid">
-                {/* <img src={logo} className="App-logo" height="45px" width="150px" alt="logo" /> */}
-                     <a href="j4f" className="navbar-brand mr-3"><h1><strong>J4F</strong></h1></a> 
-    
-
-                            {/* <div className="collapse navbar-collapse" id="navbarCollapse">
-                                <div className="navbar-nav">
-                                    {/* <a href="/" className="nav-item nav-link active"></a> */}
-                                    {/* <a href="src/staff" className="nav-item nav-link active">STAFF</a>
-                                    <a href="partnerships" className="nav-item nav-link">PARTNERSHIPS</a>
-                                    <a href="institute" className="nav-item nav-link">INSTITUTE</a>
-                                    <a href="policy" className="nav-item nav-link">POLICY</a>
-                                    <a href="trainings" className="nav-item nav-link">TRAININGS</a>
-                                </div> */}
-
-                            <div className="navbar-nav ml-auto">
-                                <a href="search" className="nav-item nav-link">Search</a>
-                                <a href="report" className="nav-item nav-link">Download Report</a>
-                            </div>
-                        </div>
-                        
-            </nav>
-    
-      <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" ></img>
+    {/* <a href="j4f" className="navbar-brand mr-3"><h1><strong>J4F</strong></h1></a>  */}
+        <NavBar className="navbar navbar sticky-top navbar-expand-md bg=light variant=light mb-5" /> 
+        <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" ></img>
        
-
-         <BrowserRouter>
-        {/* <NavBar /> */}
-        <Switch>
-        <Route exact path="/" component={Home} />  
-        <Route path="/staff" component={Staff} /> 
-        <Route path="/institute" component={Institute} />
-        <Route path="/partnerships" component={Partnerships} />
-        <Route path="/policy" component={Policy} /> 
-        <Route path="/trainings" component={Trainings} />
-        
-        </Switch>
-    </BrowserRouter> 
-
-       
-          {/* Edit <code>src/App.js</code> and save to reload. */}
-        
-      {/* This area should link to the first home page of site */}
         <a
           className="App-link"
           href="j4f" 
@@ -110,20 +68,36 @@ function App() {
 
         </a>
         <div>
-		{/* <div
-			style={{
-				width: '200px',
-				height: '200px',
-				position: 'absolute',
-				top: '0px',
-				left: '0px',
-				background: 'red',
-				zIndex: 1,
-			}} */}
+	
 		
 		{/* <Animate /> */}
 	</div>
       </header>
+        <div>
+            <nav className="navbar navbar sticky-top navbar-expand-md bg=light variant=light mb-5">
+                {/* <nav className="navbar sticky-top navbar-light bg-light"> */}
+                
+                <div className="container-fluid">
+                {/* <img src={logo} className="App-logo" height="45px" width="150px" alt="logo" /> */}
+                     {/* <a href="j4f" className="navbar-brand mr-3"><h1><strong>J4F</strong></h1></a>  */}
+    
+
+                            {/* <div className="collapse navbar-collapse" id="navbarCollapse">
+                                <div className="navbar-nav">
+                                    {/* <a href="/" className="nav-item nav-link active"></a> */}
+                                    {/* <a href="src/staff" className="nav-item nav-link active">STAFF</a>
+                                    <a href="partnerships" className="nav-item nav-link">PARTNERSHIPS</a>
+                                    <a href="institute" className="nav-item nav-link">INSTITUTE</a>
+                                    <a href="policy" className="nav-item nav-link">POLICY</a>
+                                    <a href="trainings" className="nav-item nav-link">TRAININGS</a>
+                                </div> */}
+
+                           
+                        </div>
+                        
+            </nav>
+    
+     
       {/* <Jumbotron />
       <Staff />
       {/* <Board /> */}
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,22 +1,71 @@
-// import React from 'react'
-// //import Jumbotron from 'react-bootstrap/Jumbotron';
-// //import Container from 'react-bootstrap/Container';
-// //import ReactDOM from 'react-dom'
-// //import AnchorLink from 'react-anchor-link-smooth-scroll'
-// import 'bootstrap/dist/css/bootstrap.css';
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import './App.css'
-// //import logo from './logo.png';
+import React from 'react'
+//import Jumbotron from 'react-bootstrap/Jumbotron';
+//import Container from 'react-bootstrap/Container';
+//import ReactDOM from 'react-dom'
+//import AnchorLink from 'react-anchor-link-smooth-scroll'
+import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css'
+//import logo from './logo.png';
+import { Link, Switch, Route, Redirect } from 'react-router-dom';
+import Home from './Home'
+import Staff from './Staff'
+import Partnerships from './Partnerships'
+import Institute from './Institute'
+import Policy from './Policy'
+import Trainings from './Trainings'
 
 
 
 
 
                    
-// function NavBar() {
-//     return (
-//         <div>
-//             {/* <nav className="navbar navbar sticky-top navbar-expand-md bg=light variant=light mb-5">
+ function NavBar() {
+     return (
+         <div>
+          {/* <nav className="navbar navbar sticky-top navbar-expand-md bg=light variant=light mb-5"> */}
+                {/* <nav className="navbar sticky-top navbar-light bg-light"> */}
+                
+                {/* <div className="container-fluid"> */}
+           <div className="menu">
+            <ul>
+            <li> <Link to="/">Home</Link> </li>
+            <li> <Link to="/staff">Staff</Link> </li>
+            <li> <Link to="/partnerships">Partnerships</Link> </li>
+            <li> <Link to="/institute">Institute</Link> </li>
+            <li> <Link to="/policy">Policy</Link> </li>
+            <li> <Link to="/trainings">Trainings</Link> </li>
+            {/* <li> <Link to="/about">About</Link> </li> */}
+          </ul>
+      </div>
+      <div className="App-intro">
+        <Switch>
+          <Route exact path="/"  component={Home} />
+          <Route path="/staff" component={Staff} />
+          <Route path="/partnerships" component={Partnerships} />
+          <Route path="/institute" component={Institute} />
+          <Route path="/policy" component={Policy} />
+          <Route path="/trainings" component={Trainings} />
+          <Redirect to="/" />
+        </Switch>
+      </div>
+      
+    </div>
+
+
+  );
+}
+
+
+
+
+
+
+
+
+
+
+// <nav className="navbar navbar sticky-top navbar-expand-md bg=light variant=light mb-5">
 //                 {/* <nav className="navbar sticky-top navbar-light bg-light"> */}
 //                 <div className="container-fluid">
 //                 {/* <img src={logo} className="App-logo" height="45px" width="150px" alt="logo" /> */}
@@ -44,12 +93,10 @@
             
 //         </div>
         
-    
-//     )
-// }
+ 
 
    
         
-// export default NavBar
+ export default NavBar
        
             
