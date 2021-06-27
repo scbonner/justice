24bb7c5be105b02805247c46e0f02d3f875b7110
scbonner <45452011+scbonner@users.noreply.github.com>
2021-06-27 00:51:11 -0400

    updating whoweare

--- src/WhoWeAre.js
+++ src/WhoWeAre.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-//import Button from 'bootstrap/dist/css/bootstrap.css'
+import Button from 'bootstrap/dist/css/bootstrap.css'
 //import Card from 'bootstrap/dist/css/bootstrap.css'
 import './App.css'
 
@@ -16,69 +16,73 @@ export default function WhoWeAre() {
 <br />
 <br />
 
-
-{/* first card represents trainings */}
+{/* first card represents families */}
 
 <h2><strong>WHO WE ARE</strong></h2>
-<div className='container'>
-<p>Justice For Families works to transform how justice systems operate so that families have a VOICE and POWER in both how and what decisions are made. We move resources away from incarceration toward direct investments in the youth, families, and communities most harmed by failed “tough on crime” policies.</p>
+  <div className='container'>
+    <p>Justice For Families works to transform how justice systems operate so that families have a VOICE and POWER in both how and what decisions are made. We move resources away from incarceration toward direct investments in the youth, families, and communities most harmed by failed “tough on crime” policies.</p>
+      <div className="row row-cols-1 row-cols-md-4 g-4">
+        <div classNames="col">
+          <div className="card h-100">
+            <img src='https://images.pexels.com/photos/4205505/pexels-photo-4205505.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500' className="card-img-top" alt="..."></img>
+               <div className="card-body">
+                 <h5 className="card-title">FAMILIES</h5>
+                  <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+               </div>
+                <div className="card-footer">
+                  <a className="btn btn-light btn-lg" href="#speakers">Click Here</a>
+                  <Button variant="success">Success</Button>{' '}
+                </div>
+             </div>
+         </div>
 
-<div className="row row-cols-1 row-cols-md-4 g-4">
-  <div classNames="col">
-    <div className="card h-100">
-    <img src='https://images.pexels.com/photos/4205505/pexels-photo-4205505.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500' className="card-img-top" alt="..."></img>
-      
-      <div className="card-body">
-        <h5 className="card-title">FAMILIES</h5>
-        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      </div>
-      <div className="card-footer">
-      <a className="btn btn-light btn-lg" href="#speakers">Click Here</a>
-      </div>
-    </div>
-  </div>
+  {/* first card represents partnerships */}
 
   <div className="col">
     <div className="card h-100">
       <img src='https://images.pexels.com/photos/3184434/pexels-photo-3184434.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500' className="card-img-top" alt="..."></img>
-      <div className="card-body">
-        <h5 className="card-title">PARTNERSHIPS</h5>
-        <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-      </div>
-      <div className="card-footer">
-      <i className="fas fa-users"></i>
-      <a className="btn btn-light btn-lg" href="#speakers">Click Here</a>
-      </div>
+        <div className="card-body">
+          <h5 className="card-title">PARTNERSHIPS</h5>
+          <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+        </div>
+          <div className="card-footer">
+          {/* <i className="fas fa-users"></i> */}
+            <a className="btn btn-light btn-lg" href="#speakers">Click Here</a>
+          </div>
     </div>
   </div>
+
+{/* first card represents policy */}
+
   <div className="col">
     <div className="card h-100">
       <img src='https://images.pexels.com/photos/5668473/pexels-photo-5668473.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500' className="card-img-top" alt="..."></img>
-      <div className="card-body">
-        <h5 className="card-title">POLICY</h5>
-        <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-      </div>
-      <div className="card-footer">
-      <a className="btn btn-light btn-lg" href="#speakers">Click Here</a>
-      </div>
+        <div className="card-body">
+          <h5 className="card-title">POLICY</h5>
+          <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+        </div>
+          <div className="card-footer">
+            <a className="btn btn-light btn-lg" href="#speakers">Click Here</a>
+          </div>
     </div>
   </div>
+
+  {/* first card represents community */}
+
   <div className="col">
     <div className="card h-100">
       <img src='https://images.pexels.com/photos/6257110/pexels-photo-6257110.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500' className="card-img-top" alt="..."></img>
-      <div className="card-body">
-        <h5 className="card-title">COMMUNITY</h5>
-        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-      </div>
-      <div className="card-footer">
-      <a className="btn btn-light btn-lg" href="#speakers">Click Here</a>
+        <div className="card-body">
+          <h5 className="card-title">COMMUNITY</h5>
+          <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+        </div>
+          <div className="card-footer">
+           <a className="btn btn-light btn-lg" href="#speakers">Click Here</a>
+          </div>
       </div>
     </div>
-  </div>
-</div> 
+  </div> 
 </div>
-
-
 <br />
 <br />
 <hr />
