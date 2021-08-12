84a2de375871c592fd3d183666185e8fd3e4cb8e
scbonner <45452011+scbonner@users.noreply.github.com>
2021-08-12 07:24:43 -0400

    almost finish with media page

--- src/Media.js
+++ src/Media.js
@@ -1,76 +1,98 @@
 import React from 'react'
 
 function Media() {
-    return (
-        <div>
-          <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"/>
-<div clasNames="jumbotron">
-  <div className="container">
-    <div className="row">
-      <div className="col-xs-offset-2 col-xs-8">
-        <h1>Fathers Advocating for Reform</h1>
-        <p>Justice For Families invites you to hear this inspirational webinar with fathers advocating for justice!  Guests will hear from Elias (Eli) Gonzales, Mark Rolan, Eddie Torres and Bryce Knox all who have years of experience advocating for reforms that benefits families and young people.  This amazing group of fathers, from around the country, are working with state-based agencies, including law enforcement, probation, mental health and juvenile/family courts.</p>
-        <p><a className="btn btn-primary btn-lg" href="https://www.youtube.com/watch?v=bSsJ732yJyE" role="button">Click Start Video</a></p>
-      </div>
-      <div className="col-xs-2">
-        <img className="img-responsive" src="https://external-iad3-2.xx.fbcdn.net/safe_image.php?d=AQHDKiVunvTdt_is&w=476&h=249&url=https%3A%2F%2Fi.ytimg.com%2Fvi%2FbSsJ732yJyE%2Fhqdefault.jpg&cfs=1&upscale=1&fallback=news_d_placeholder_publisher&ccb=3-5&_nc_hash=AQGmLwthaxYtXjF1" alt=""></img>
-      </div>
-    </div>
-  </div>
-</div>
+  return (
 
-
-
-<div className="container bg-dark text-white border py-3 my-3">
-    <h3>OJJDP - Office of Juvenile Justice and Delinquency Prevention</h3>
-        <h4>Data Snapshots</h4>
-            <a href="https://ojjdp.ojp.gov/library/publications/decline-arrests-juveniles-continued-through-2019" rel="stylesheet">"The Decline in Arrests of Juveniles Continued Through 2019"</a> <br />
-
-            <a href="https://ojjdp.ojp.gov/library/publications/highlights-2018-juvenile-residential-facility-census" rel="stylesheet">"Highlights From the 2018 Juvenile Residential Facility Census"</a> <br />
-
-            <a href="https://ojjdp.ojp.gov/library/publications/arrests-juveniles-2018-reached-lowest-level-nearly-4-decades" rel="stylesheet">"Arrests of Juveniles in 2018 Reached Lowest Level in Nearly 4 Decades"</a> <br />
-
-            <a href="https://ojjdp.ojp.gov/library/publications/characteristics-and-trends-delinquency-cases-resulting-probation" rel="stylesheet">"Characteristics and Trends of Delinquency Cases Resulting in Probation"</a> <br />
-
-            <a href="https://ojjdp.ojp.gov/library/publications/trends-and-characteristics-youth-residential-placement-2017" rel="stylesheet">"Trends and Characteristics of Youth in Residential Placement, 2017"</a> <br />
-
-            <a href="https://ojjdp.ojp.gov/library/publications/characteristics-cases-judicially-waived-juvenile-criminal-court" rel="stylesheet">"Characteristics of Cases Judicially Waived From Juvenile to Criminal Court"</a> <br />
-
-            <a href="https://ojjdp.ojp.gov/library/publications/arrest-characteristics-older-juveniles-and-young-adults" rel="stylesheet">"Arrest Characteristics of Older Juveniles and Young Adults"</a> <br />
-
-            <a href="https://ojjdp.ojp.gov/library/publications/characteristics-delinquency-cases-handled-juvenile-court-2016" rel="stylesheet">"Characteristics of Delinquency Cases Handled in Juvenile Court 2016"</a> <br />
-
+<>
+<div className='container'>
+  <h2>Webinars</h2>
+    {/* <div id='triangle1' className='webinar'> */}
+    <br />
+      <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"/>
+        <div clasNames="jumbotron">
+          <div className="container">
+            <div className="row">
+              <div className="col-xs-offset-2 col-xs-8">
+                <h2><u>Fathers Advocating for Reform</u></h2>
+                <p>Justice For Families invites you to hear this inspirational webinar with fathers advocating for justice!  Guests will hear from Elias (Eli) Gonzales, Mark Rolan, Eddie Torres and Bryce Knox all who have years of experience advocating for reforms that benefits families and young people.  This amazing group of fathers, from around the country, are working with state-based agencies, including law enforcement, probation, mental health and juvenile/family courts.</p>
+                <p><a className="btn btn-primary btn-lg" href="https://www.youtube.com/watch?v=bSsJ732yJyE" role="button">Click Start Video</a></p>
+              </div>
+              <div className="col-xs-2">
+                <img className="img-responsive" src="https://external-iad3-2.xx.fbcdn.net/safe_image.php?d=AQHDKiVunvTdt_is&w=476&h=249&url=https%3A%2F%2Fi.ytimg.com%2Fvi%2FbSsJ732yJyE%2Fhqdefault.jpg&cfs=1&upscale=1&fallback=news_d_placeholder_publisher&ccb=3-5&_nc_hash=AQGmLwthaxYtXjF1" alt=""></img>
+              </div>
             </div>
+          </div>
+          <hr />
+        </div>
+      <br />
 
-            <div className="container bg-dark text-white border py-3 my-3">
-    <h3>Vera Institute of Racial Justice</h3> <br />
+<div>
+  <h2>Information & Data Links</h2>
+
+<div className="container bg-secondary text-white border py-3 my-3">
+  <h3><center>OJJDP - Office of Juvenile Justice and Delinquency Prevention</center></h3><br></br>
+    <h4>Data Snapshots</h4>
+      <a href="https://ojjdp.ojp.gov/library/publications/decline-arrests-juveniles-continued-through-2019" rel="stylesheet">"The Decline in Arrests of Juveniles Continued Through 2019"</a> <br />
+
+      <a href="https://ojjdp.ojp.gov/library/publications/highlights-2018-juvenile-residential-facility-census" rel="stylesheet">"Highlights From the 2018 Juvenile Residential Facility Census"</a> <br />
+
+      <a href="https://ojjdp.ojp.gov/library/publications/arrests-juveniles-2018-reached-lowest-level-nearly-4-decades" rel="stylesheet">"Arrests of Juveniles in 2018 Reached Lowest Level in Nearly 4 Decades"</a> <br />
+
+      <a href="https://ojjdp.ojp.gov/library/publications/characteristics-and-trends-delinquency-cases-resulting-probation" rel="stylesheet">"Characteristics and Trends of Delinquency Cases Resulting in Probation"</a> <br />
+
+      <a href="https://ojjdp.ojp.gov/library/publications/trends-and-characteristics-youth-residential-placement-2017" rel="stylesheet">"Trends and Characteristics of Youth in Residential Placement, 2017"</a> <br />
+
+      <a href="https://ojjdp.ojp.gov/library/publications/characteristics-cases-judicially-waived-juvenile-criminal-court" rel="stylesheet">"Characteristics of Cases Judicially Waived From Juvenile to Criminal Court"</a> <br />
+
+      <a href="https://ojjdp.ojp.gov/library/publications/arrest-characteristics-older-juveniles-and-young-adults" rel="stylesheet">"Arrest Characteristics of Older Juveniles and Young Adults"</a> <br />
+
+      <a href="https://ojjdp.ojp.gov/library/publications/characteristics-delinquency-cases-handled-juvenile-court-2016" rel="stylesheet">"Characteristics of Delinquency Cases Handled in Juvenile Court 2016"</a> <br />  <br />
+    </div>
+
+    <br />
+{/* Vera Instistute */}
+
+    <div className="container bg-dark text-white border py-3 my-3">
+      <h3><center>Vera Institute of Racial Justice</center></h3> <br />
         <h4>A Look at Spending in America’s Largest Cities</h4>
-            <a href="https://www.vera.org/publications/what-prosecution-costs" rel="stylesheet">"What Prosecution Costs"</a> <br />
+          <a href="https://www.vera.org/publications/what-prosecution-costs" rel="stylesheet">"What Prosecution Costs"</a> <br />
+
+        <h4>Compassion, Not Confinement</h4>
+            <a href="https://www.vera.org/publications/compassion-not-confinement" rel="stylesheet">"Explore Resources"</a> <br />
+
+        <h4>People in Jail and Prison in Spring 2021</h4>
+            <a href="https://www.vera.org/publications/what-prosecution-costs" rel="stylesheet">"How States and Localities Can Help Ensure Humane Housing for Unaccompanied Immigrant Children Awaiting Family Reunification"</a> <br />
+        <h4>Arrest Trends</h4>
+            <a href="https://arresttrends.vera.org" rel="stylesheet">"Did You Know? An arrest is made every three seconds", etc...</a> <br />  <br />
+    </div>
+    <br />
+{/* ANNE E CASEY FOUNDATION */}
+    <div className="container bg-dark text-white border py-3 my-3">
+      <h3><center>The Annie E. Casey Foundation</center></h3>
+        <h4>Data Book Extras</h4>
+          <h4>2021 KIDS COUNT Data Book Interactive</h4>
+            <a href="https://www.aecf.org/interactive/databook" rel="stylesheet">"Interactive"</a> <br />
 
             {/* <div className="container bg-dark text-white border py-3 my-3"> */}
 
-            <h4>Compassion, Not Confinement</h4>
-            <a href="https://www.vera.org/publications/compassion-not-confinement" rel="stylesheet">"Explore Resources"</a> <br />
+          <h4>National News Release</h4>
+            <a href="https://www.aecf.org/blog/50-state-data-report-confirms-urgent-need-to-make-game-changing-expanded-child-tax-credit-permanent" rel="stylesheet">"50-State Data Report Confirms Urgent Need to Make Game-Changing Expanded Child Tax Credit Permanent"</a> <br />
+          <h4>State News Releases</h4>
+            <a href="https://www.aecf.org/resources/2021-kids-count-data-book#Family%20and%20Community" rel="stylesheet">"2021 KIDS COUNT DATA BOOK - 2021 State Trends in Child Well-Being"</a> <br />
+
+          <h4>2021 State Data Profiles on Kids' Well-Being</h4>
+            <a href="https://www.aecf.org/resources/2021-kids-count-data-book#2021%20State%20Data%20Profiles%20on%20Kids%27%20Well-Being" rel="stylesheet">"2021 State Trends in Child Well-Being"</a> <br />
 
 
-        <h4>People in Jail and Prison in Spring 2021</h4>
-            <a href="https://www.vera.org/publications/what-prosecution-costs" rel="stylesheet">"How States and Localities Can Help Ensure Humane Housing for Unaccompanied Immigrant Children Awaiting Family Reunification
-"</a> <br />
-
-        <h4>Arrest Trends</h4>
-            <a href="https://arresttrends.vera.org" rel="stylesheet">"Did You Know? An arrest is made every three seconds", etc...</a> <br />
- </div>
-
-
- {/* <div className="container bg-dark text-white border py-3 my-3">
-    <h3>OJJDP - Office of Juvenile Justice and Delinquency Prevention</h3>
-        <h4>Data Snapshots</h4>
-            <a href="https://ojjdp.ojp.gov/library/publications/highlights-2018-juvenile-residential-facility-census" rel="stylesheet">"Highlights From the 2018 Juvenile Residential Facility Census"</a> */}
-        {/* <p>This is a paragraph of text.</p> */}
-   
+          <h4>Trends and Overall Child Well-Being</h4>
+            <a href="https://www.aecf.org/resources/2021-kids-count-data-book#Trends%20and%20Overall%20Child%20Well-Being">"Summary"</a> <br />  <br />
+        </div>
+      </div>
     </div>
+    <br />
+  
 
-
+</>
 
     
     )
